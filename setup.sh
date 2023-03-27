2a535c281efe999192ef5c8ba5c21b362a84e92d
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2023-03-27 14:22:19 -0400

    [SETUP] simpler script

--- setup.sh
+++ setup.sh
@@ -18,23 +18,22 @@ function setup() {
     # Install all packages
     brew bundle install --file brew/Brewfile
 
-    pushd user > /dev/null || exit 1
-
-    for dotfile in .*; do
-        if [[ ! "$dotfile" =~ .DS_Store$|.git$|^.$|^..$ ]]; then
-                # Backup the file if it is present
-                if [[ -f "$HOME/$dotfile" ]] || [[ -d "$HOME"/$dotfile ]] ;then
-                    echo "[INFO] backing up original $dotfile"
-                    [[ -f "$HOME/$dotfile.backup" ]] && rm "$HOME/$dotfile.backup"
-                    [[ -d "$HOME/$dotfile.backup" ]] && rm "$HOME/$dotfile.backup"
-                    mv "$HOME/$dotfile" "$HOME/$dotfile.backup"
-                fi
-                # Create a symlink to the .dotfile in this folder
-                ln -snf "$DOTFILES/user/$dotfile" "$HOME/$dotfile";
+    for USER_FILE in user/.*; do
+        local DOTFILE="$(basename $USER_FILE)"
+        if [[ ! "$DOTFILE" =~ .DS_Store$|.git$|^.$|^..$ ]]; then
+            # Backup the file if it is present
+            if [[ -f "$HOME/$DOTFILE" ]] || [[ -d "$HOME"/$DOTFILE ]] ;then
+                # Discard previous backups if they were there
+                [[ -f "$HOME/$DOTFILE.backup" ]] || [[ -d "$HOME/$DOTFILE.backup" ]] && rm "$HOME/$DOTFILE.backup"
+                # Copy the current file to its backup
+                echo "[INFO] backing up original $DOTFILE"
+                mv "$HOME/$DOTFILE" "$HOME/$DOTFILE.backup"
+            fi
+            # Create a symlink to the .dotfile in this folder
+            ln -snf "$DOTFILES/$USER_FILE" "$HOME/$DOTFILE";
         fi;
     done
 
-    popd > /dev/null || exit 1
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
