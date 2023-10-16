2200eba27ad90ac7b66cbbe7b12191969f069939
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2023-10-16 14:47:14 -0400

    [THEME] horizon everything

--- user/.config/nvim/lua/plugins/colorscheme.lua
+++ user/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,55 +1,16 @@
--- Remove warning about transparent_background
-require("notify").setup({
-  background_colour = "#000000",
-})
-
 return {
   -- add theme
   {
-    "catppuccin/nvim",
+    "akinsho/horizon.nvim",
     lazy = true,
-    name = "catppuccin",
-
-    opts = {
-      term_colors = false,
-      transparent_background = true,
-      integrations = {
-        alpha = true,
-        cmp = true,
-        flash = true,
-        gitsigns = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        lsp_trouble = true,
-        mason = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        noice = true,
-        notify = true,
-        neotree = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-      },
-    },
+    name = "horizon",
   },
 
   -- Configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "horizon",
     },
   },
 }
