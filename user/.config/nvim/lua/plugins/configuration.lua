b4f080ae18e99e98796016c4db21be9873b6a334
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2023-10-17 22:40:53 -0400

    [NVIM] better plugin management

---
+++ user/.config/nvim/lua/plugins/configuration.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+            "thumbs.db",
+          },
+          never_show = {},
+        },
+      },
+    },
+  },
+}
