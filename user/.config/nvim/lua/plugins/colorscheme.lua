9d8ec63370f657bc844ff5af52911dff45ca1759
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2023-10-23 12:54:13 -0400

    [NVIM] transparency

--- user/.config/nvim/lua/plugins/colorscheme.lua
+++ user/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,17 @@
+-- Remove warnings for transparent background
+require("notify").setup({
+  background_colour = "#000000",
+})
+
 return {
-  -- add theme
+  -- Set up transparent background
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    name = "transparent",
+  },
+
+  -- Add colorscheme
   {
     "akinsho/horizon.nvim",
     lazy = true,
