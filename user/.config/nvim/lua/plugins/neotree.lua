c020111eb70dd9f9516aab8e1c15292d4e045cf7
d-asnaghi <30296575+hexdae@users.noreply.github.com>
2025-01-14 18:21:33 -0500

    [NVIM] neotree open

--- user/.config/nvim/lua/plugins/neotree.lua
+++ user/.config/nvim/lua/plugins/neotree.lua
@@ -16,6 +16,27 @@ return {
           never_show = {},
         },
       },
+      mappings = {
+        ["O"] = {
+          command = function(state)
+            local node = state.tree:get_node()
+            local filepath = node.path
+            local osType = os.getenv("OS")
+
+            local command
+
+            if osType == "Windows_NT" then
+              command = "start " .. filepath
+            elseif osType == "Darwin" then
+              command = "open " .. filepath
+            else
+              command = "xdg-open " .. filepath
+            end
+            os.execute(command)
+          end,
+          desc = "open_with_system_defaults",
+        },
+      },
     },
   },
 }
